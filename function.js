0571f5f33ae1e1555ac00dabed89605aa2b95bc1
AkhilaReddy <akhilagreddy.22@gmail.com>
2025-09-17 19:59:43 +0530

    Basics

---
+++ function.js
@@ -0,0 +1,12 @@
+//function is a set of statements used to perform some specific and repeated tasks
+//syntax : functtion name(){
+//}
+
+
+//function to add two numbers
+function add(a,b){
+    return a+b;
+}
+const c=10 , d =20;
+const sum = add(c,d);
+console.log(sum);
